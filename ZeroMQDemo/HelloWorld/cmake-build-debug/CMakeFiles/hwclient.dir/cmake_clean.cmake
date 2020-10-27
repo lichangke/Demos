217ad47cd1ffcc3a82e3ed4449d1a41e1495d811
lichangke <986740304@qq.com>
2020-10-27 22:52:33 +0800

    add helloworld

---
+++ ZeroMQDemo/HelloWorld/cmake-build-debug/CMakeFiles/hwclient.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hwclient.dir/hwclient.c.o"
+  "hwclient"
+  "hwclient.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hwclient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
