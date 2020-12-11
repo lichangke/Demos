368e42911d4806d27558989275a85c4b8999bd0c
lichangke <986740304@qq.com>
2020-12-11 09:57:57 +0800

    Update main.cpp

--- IOMultiplexingDemo/EpollDemo/main.cpp
+++ IOMultiplexingDemo/EpollDemo/main.cpp
@@ -11,18 +11,23 @@ int main ()
     char c;
 
     // 键盘输入
-    keyboard = open("/dev/tty",O_RDONLY | O_NONBLOCK); // /dev/tty
+    keyboard = open("/dev/tty",O_RDONLY | O_NONBLOCK);
     assert(keyboard>0);
 
+    int epoll_fd=epoll_create(10);
     //step1: epoll开始工作之前 先把文件描述符纳入epoll监管
+
+    ///  场景1
+    struct epoll_event  listen_fd_0{};
+    listen_fd_0.data.fd = 0;
+    listen_fd_0.events = EPOLLIN;
+    ret = epoll_ctl(epoll_fd,EPOLL_CTL_ADD,0,&listen_fd_0);
+
+    ///  场景2
     struct epoll_event  listen_fd{};
     listen_fd.data.fd = keyboard;
     listen_fd.events = EPOLLIN;
-    int epoll_fd=epoll_create(10);
-	
-    //epoll_ctl(epoll_fd,EPOLL_CTL_ADD,0,&listen_fd);  // 0 是标准输入 stdin
-	epoll_ctl(epoll_fd,EPOLL_CTL_ADD,keyboard,&listen_fd);
-	
+    ret = epoll_ctl(epoll_fd,EPOLL_CTL_ADD,keyboard,&listen_fd);
 
     struct epoll_event ready_events[10];
     bool flag = true;
@@ -37,7 +42,18 @@ int main ()
             //step3:epoll完成工作  看自己感兴趣的套接字是否就绪
             for(int i=0;i<ret;i++){
                 if(ready_events[i].data.fd==keyboard){
+                    printf("data.fd==keyboard\n");
                     i=read(keyboard,&c,1); // 读取数据
+                    if('\n'==c)
+                        continue;
+                    if ('q'==c) {
+                        flag = false;
+                        break;
+                    }
+                    printf("the input is %c\n",c);
+                } else if (ready_events[i].data.fd==0) {
+                    printf("data.fd==0\n");
+                    i=read(0,&c,1); // 读取数据
                     if('\n'==c)
                         continue;
                     if ('q'==c) {
